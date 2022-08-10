ec5724b506e6db3c499edfad6fcd31c1d2d7d378
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-10 12:17:52 -0400

    excluding a category

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  #validates :category, %i[chinese, italian, japanese, french, belgian]
+  validates :category, exclusion: { in: %w(neptunian) }
 end
