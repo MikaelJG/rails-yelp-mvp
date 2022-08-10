14871ca36b996d2074bd7ad59f48f349162ee617
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-10 14:20:58 -0400

    methods done, but create

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,44 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def delete
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
